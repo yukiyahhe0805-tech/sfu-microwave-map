3798c44bb08188bdb02d0e473bc5efab303e406a
Yuki Ko <yukiko@Yukis-Mac-mini.local>
2025-10-02 13:44:13 -0700

    change how the maps look

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -53,11 +53,12 @@ export default function MapView() {
             >
                 {/* Clean Carto light basemap */}
                 <TileLayer
-                    url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                    url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
                     subdomains="abcd"
                     maxZoom={20}
                 />
+
 
                 {/* Microwave markers */}
                 {microwaves.map((m, i) => (
